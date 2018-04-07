14f8f9cceb516651cd877f6ee8563e35c8dd0f84
Victor Truong <me@ifvictr.com>
2018-04-07 21:37:52 +0000

    Switch domain precedence

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 8080;
 const OLD_DOMAIN = process.env.OLD_DOMAIN || "goo.gl";
 const NEW_DOMAIN = process.env.NEW_DOMAIN || "ifvr.co";
 
-app.get("/:id?", async (req, res, next) => {
+app.get("/:id?", async (req, res) => {
     const id = req.params.id;
     let domain;
 
     try {
-        await got(`https://${OLD_DOMAIN}/${id}`); // Throws an error on 404
-        domain = OLD_DOMAIN;
+        await got(`https://${NEW_DOMAIN}/${id}`); // Throws an error on 404
+        domain = NEW_DOMAIN;
     }
     catch(e) {
-        domain = NEW_DOMAIN;
+        domain = OLD_DOMAIN;
     }
 
     res.redirect(`https://${domain}/${id}`);
